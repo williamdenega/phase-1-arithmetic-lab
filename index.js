2672854b3a7cf141c75f5aa1d6580739513a6218
WilliamDenega <williamdenega@gmail.com>
2021-11-15 15:42:25 -0500

    commit tree 3c367cc7aad1fb8bbec8780d48d00fb14e74bb8a

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+
+//multiply
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+
+//random
+const random = Math.floor( Math.random()*1000);
+
+
+//mod
+const num3 = 60;
+const num4 = 7;
+const mod = num3 % num4;
+
+//max
+
+const max = Math.max(6,1,15,20);
